a99247096e7e9c609ac8dbbf1d9e93fa91341d60
lewy192 <lewis.james.hill@outlook.com>
2021-07-10 16:23:52 +1000

    add:about section tablet and up completed

--- assets/js/size.js
+++ assets/js/size.js
@@ -1,10 +1,16 @@
 const { width } = screen;
 
 const typeWriterContainer = document.querySelector(".color");
+const aboutContainer = document.querySelector(".about-container");
 
 function changeLabels() {
     if (width <= 768) {
         typeWriterContainer.remove();
+    }
+    if (width >= 768) {
+        const aboutPhotoContainer = document.createElement("div");
+        aboutPhotoContainer.className = "about-image";
+        aboutContainer.appendChild(aboutPhotoContainer);
     }
 }
 
