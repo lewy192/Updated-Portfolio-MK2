554f14dd48b9e8fc5c5a029de5190658649ca936
lewy192 <lewis.james.hill@outlook.com>
2021-07-10 14:00:55 +1000

    update: profile for mobile complete

--- assets/js/size.js
+++ assets/js/size.js
@@ -1,13 +1,10 @@
 const { width } = screen;
-const openLabel = document.querySelector(".open-profile");
-const closeLabel = document.querySelector(".close-profile");
-const profileSection = document.querySelector(".section1");
-const profileContainer = document.getElementById("profile-container");
+
+const typeWriterContainer = document.querySelector(".color");
 
 function changeLabels() {
     if (width <= 768) {
-        profileSection.insertBefore(openLabel, profileContainer);
-        profileSection.appendChild(closeLabel);
+        typeWriterContainer.remove();
     }
 }
 
